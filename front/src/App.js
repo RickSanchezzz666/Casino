06749e2f5254df913531da5243f48479b6e0e731
RickSanchezzz666 <lapiyktube@gmail.com>
2023-06-28 11:04:24 +0300

    real chances

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
 import './App.css';
 import MainPage from "./Components/mainPage";
+import Roulette from "./Components/roulette";
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+
 
 function App() {
   return (
-    <MainPage />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/game" element={<Roulette />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
